7a30e1d64ad0e69e051d33fb0d62111413104c26
Dario Vettura <73988414+dariovettura@users.noreply.github.com>
2023-01-15 14:01:50 +0100

    ADD

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,22 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
 
-  
+
   return (
-    <div>
-       <Link href="/video-page">
-          <a>video</a>
-        </Link>
-  
+    <div style={{
+      width: "100vw",
+      heigth: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      gap: "10px",
+      alignItem: "center",
+      justifyContent: "center"
+    }}>
+      <span>TIK TOK UI VIDEO</span>
+      <Link href="/video-page">
+        <a>Go to video</a>
+      </Link>
+
     </div>
   )
 }
